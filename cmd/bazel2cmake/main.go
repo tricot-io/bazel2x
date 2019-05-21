f9d017eb1cee9721183d795ff1bd0720f5c64680
Viet-Trung Luu <vtl@tricot.io>
2019-05-20 23:50:10 -0700

    More WIP.

--- cmd/bazel2cmake/main.go
+++ cmd/bazel2cmake/main.go
@@ -6,24 +6,68 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"bazel2cmake/bazel"
 	"bazel2cmake/utils"
 )
 
 func main() {
-	cwd, err := os.Getwd()
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Print("ERROR: BUILD[.bazel] argument(s) required\n")
+		os.Exit(1)
+	}
+
+	// Use the first arg to determine the workspace root.
+	workspaceDir, _, err := utils.FindWorkspaceDir(filepath.Dir(args[0]))
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Workspace root: %s\n", workspaceDir)
+
+	projectName := filepath.Base(workspaceDir)
+	if projectName == string(filepath.Separator) {
+		fmt.Printf("ERROR: unable to determine project name\n")
+		os.Exit(1)
+	}
+	fmt.Printf("Project name: %s\n", projectName)
+
+	// Preprocess all the arguments to get relative paths to the workspace dir, since we'll
+	// Chdir to the workspace dir.
+	buildFileLabels := make([]bazel.Label, len(args))
+	for i, arg := range args {
+		wsDir, relDir, err := utils.FindWorkspaceDir(filepath.Dir(arg))
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+			os.Exit(1)
+		}
+
+		if wsDir != workspaceDir {
+			fmt.Printf("ERROR: %s not in same workspace as %s\n", arg, args[0])
+			os.Exit(1)
+		}
+
+		buildFileLabels[i] = bazel.Label{
+			Workspace: "",
+			Package:   bazel.PackageName(relDir),
+			Target:    bazel.TargetName(filepath.Base(arg)),
+		}
+
+		fmt.Printf("Input BUILD file: %s\n", buildFileLabels[i])
+	}
+
+	err = os.Chdir(workspaceDir)
 	if err != nil {
 		fmt.Printf("ERROR: %v\n", err)
 		os.Exit(1)
 	}
 
-	absdir, reldir, err := utils.FindWorkspaceDir(cwd)
-	if err != nil {
-		fmt.Printf("ERROR: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("absdir=%q, reldir=%q\n", absdir, reldir)
+	// TODO(vtl)
 }
